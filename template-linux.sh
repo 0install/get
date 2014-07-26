faacf5b2e7b72929844ce2b4c7bd5fc894b452d7
Bastian Eicher <bastian@eicher.net>
2014-07-26 15:38:50 +0200

    Shell script: misc tweaks

--- template-linux.sh
+++ template-linux.sh
@@ -5,25 +5,25 @@ APP_NAME="--------------------AppName--------------------"
 APP_URI="----------------------------------------AppUri----------------------------------------"
 
 if command -v 0install > /dev/null; then
-  read -p "This will install $APP_NAME using Zero Install."
+  read -p "This will install $APP_NAME using Zero Install. Press ENTER to continue."
 elif command -v emerge > /dev/null; then
-  read -p "This will first install Zero Install using emerge and then install $APP_NAME using Zero Install."
+  read -p "This will first install Zero Install using emerge and then install $APP_NAME using Zero Install. Press ENTER to continue."
   emerge zeroinstall-injector
 elif command -v apt-get > /dev/null; then
-  read -p "This will first install Zero Install using apt-get and then install $APP_NAME using Zero Install."
+  read -p "This will first install Zero Install using apt-get and then install $APP_NAME using Zero Install. Press ENTER to continue."
   apt-get install zeroinstall-injector
 elif command -v yast > /dev/null; then
-  read -p "This will first install Zero Install using yast and then install $APP_NAME using Zero Install."
+  read -p "This will first install Zero Install using yast and then install $APP_NAME using Zero Install. Press ENTER to continue."
   yast -i zeroinstall-injector
 elif command -v urpmi > /dev/null; then
-  read -p "This will first install Zero Install using urpmi and then install $APP_NAME using Zero Install."
+  read -p "This will first install Zero Install using urpmi and then install $APP_NAME using Zero Install. Press ENTER to continue."
   urpmi zeroinstall-injector
 elif command -v yum > /dev/null; then
-  read -p "This will first install Zero Install using yum and then install $APP_NAME using Zero Install."
+  read -p "This will first install Zero Install using yum and then install $APP_NAME using Zero Install. Press ENTER to continue."
   yum install zeroinstall-injector
 else
   echo "Zero Install is not installed on this system. Please visit http://0install.net/install-linux.html for instructions."
-  exit
+  exit 1
 fi
 
 0desktop $APP_URI
