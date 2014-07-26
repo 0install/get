c4edc110a0acd5586570a48f0457b159ccf4b795
Bastian Eicher <bastian@eicher.net>
2014-07-26 15:13:18 +0200

    Shell script: fixed package manager detection

--- template-linux.sh
+++ template-linux.sh
@@ -1,24 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # Constants
 APP_NAME="--------------------AppName--------------------"
 APP_URI="----------------------------------------AppUri----------------------------------------"
 
-echo "This will first install Zero Install using your native package manager and then install $APP_NAME"
-pause
-
-if command pacman > /dev/null; then
-  pacman -S zeroinstall-injector
-elif command emerge > /dev/null; then
+if command -v 0install > /dev/null; then
+  read -p "This will install $APP_NAME using Zero Install."
+elif command -v emerge > /dev/null; then
+  read -p "This will first install Zero Install using emerge and then install $APP_NAME using Zero Install."
   emerge zeroinstall-injector
-elif command apt-get > /dev/null; then
+elif command -v apt-get > /dev/null; then
+  read -p "This will first install Zero Install using apt-get and then install $APP_NAME using Zero Install."
   apt-get install zeroinstall-injector
-elif command yast > /dev/null; then
+elif command -v yast > /dev/null; then
+  read -p "This will first install Zero Install using yast and then install $APP_NAME using Zero Install."
   yast -i zeroinstall-injector
-elif command urpmi > /dev/null; then
+elif command -v urpmi > /dev/null; then
+  read -p "This will first install Zero Install using urpmi and then install $APP_NAME using Zero Install."
   urpmi zeroinstall-injector
-elif command yum > /dev/null; then
+elif command -v yum > /dev/null; then
+  read -p "This will first install Zero Install using yum and then install $APP_NAME using Zero Install."
   yum install zeroinstall-injector
+else
+  echo "Zero Install is not installed on this system. Please visit http://0install.net/install-linux.html for instructions."
+  exit
 fi
 
 0desktop $APP_URI
