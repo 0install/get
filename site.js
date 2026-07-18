6f3c85ed7e6d46a0100e3c5a971a434035fb63c7
Bastian Eicher <bastian@eicher.net>
2026-07-18 13:19:04 +0200

    Redesigned website

--- site.js
+++ site.js
@@ -1,32 +1,23 @@
 function is(os) {
-    return navigator.userAgent.search(os) >= 0;
+    return navigator.userAgent.indexOf(os) >= 0;
 }
 
 function detectOS() {
-    if (is('Debian') || is('Ubuntu')) { return 'linux-debian'; }
+    if (is('Debian') || is('Ubuntu')) { return 'linux-ubuntu'; }
     if (is('Fedora')) { return 'linux-fedora'; }
     if (is('Red Hat')) { return 'linux-redhat'; }
     if (is('SUSE')) { return 'linux-suse'; }
     if (is('Arch')) { return 'linux-arch'; }
     if (is('Linux')) { return 'linux-generic'; }
-    if (is('OS X')) { return 'mac-homebrew'; }    
+    if (is('OS X')) { return 'mac-homebrew'; }
     if (is('Windows')) { return 'windows'; }
     return null;
-}
-
-function hideTabs() {
-    $('.nav-tabs a.nav-link').removeClass('active');
-    $('.tab-body').hide();
-}
-
-function showTab(name) {
-    $('.nav-tabs a.nav-link[href="#' + name + '"]').addClass('active');
-    $('#' + name + '-tab').show();
 }
 
 function updateTabs() {
     hideTabs();
 
+    var tabName;
     if (window.location.hash) {
         tabName = window.location.hash.slice(1);
         if (tabName == 'linux') { tabName = 'linux-generic'; }
@@ -41,18 +32,12 @@ function updateTabs() {
     showTab(tabName);
 }
 
-function copy(id) {
-    $('#' + id).select();
-    document.execCommand('copy');
-}
+addEventListener('hashchange', updateTabs);
+updateTabs();
 
-$(window).bind('hashchange', updateTabs);
-$(function() {
-    updateTabs();
-    if (is('Windows NT 6.2') || is('Windows NT 6.3') || is('Windows NT 10.0')) {
-        $('#windows-tls-notice').hide();
-    }
-    if (is('Windows NT 10.0')) {
-        $('#windows-netframework-notice').hide();
-    }
-});
+if (is('Windows NT 6.2') || is('Windows NT 6.3') || is('Windows NT 10.0')) {
+    document.getElementById('windows-tls-notice').hidden = true;
+}
+if (is('Windows NT 10.0')) {
+    document.getElementById('windows-netframework-notice').hidden = true;
+}
